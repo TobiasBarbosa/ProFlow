8d5ffb65acbc9c2b5b7dac978132106ba7fddd44
“TobiasBarbosa” <toha0006@stud.kea.dk>
2024-11-26 12:56:56 +0100

    model klasser rettelser (fra abstract til ikke at være abstract)

---
+++ src/main/java/org/example/proflow/service/ProfileService.java
@@ -0,0 +1,18 @@
+package org.example.proflow.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProfileService {
+
+    //***ATTRIBUTES***--------------------------------------------------------------------------------------------------
+    //ProfileRepository profileRepository;
+
+    //***CONSTRUCTORS***------------------------------------------------------------------------------------------------
+//    public ProfileService(ProfileRepository profileRepository){
+//        this.profileRepository = profileRepository;
+//    }
+    //***METHODS***-----------------------------------------------------------------------------------------------------
+
+    //**END***----------------------------------------------------------------------------------------------------------
+}
---
+++ src/main/java/org/example/proflow/service/TaskService.java
@@ -0,0 +1,18 @@
+package org.example.proflow.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaskService {
+
+    //***ATTRIBUTES***--------------------------------------------------------------------------------------------------
+    //TaskRepository taskRepository;
+
+    //***CONSTRUCTORS***------------------------------------------------------------------------------------------------
+//    public TaskService(TaskRepository taskRepository){
+//        this.taskRepository = taskRepository;
+//    }
+    //***METHODS***-----------------------------------------------------------------------------------------------------
+
+    //**END***----------------------------------------------------------------------------------------------------------
+}
