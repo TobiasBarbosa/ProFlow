9b76eeace669967859d53918a7dd9d139e0c8791
JosefineChristine <joro0003@stud.kea.dk>
2024-12-09 20:47:08 +0100

    Lavet ProfileServiceTest og prøvet men udkommenteret ProfileControllerTest.java

--- src/test/java/org/example/proflow/controllerTest/ProfileControllerTest.java
+++ src/test/java/org/example/proflow/controllerTest/ProfileControllerTest.java
@@ -1,6 +1,7 @@
 package org.example.proflow.controllerTest;
 
 import org.example.proflow.controller.ProfileController;
+import org.example.proflow.exception.ProfileException;
 import org.example.proflow.model.Profile;
 import org.example.proflow.service.ProfileService;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,13 +11,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 //import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-@ExtendWith(SpringExtension.class)
-@WebMvcTest(ProfileController.class)
+import java.sql.SQLException;
 
-public class ProfileControllerTest {
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+//
+//@ExtendWith(SpringExtension.class)
+//@WebMvcTest(ProfileController.class)
+//
+//public class ProfileControllerTest {
 //    @Autowired
 //    private MockMvc mockMvc;
 //
@@ -27,8 +35,19 @@ public class ProfileControllerTest {
 //
 //    @BeforeEach
 //    public void setUp() {
-//        profile = new Profile(1, "Josefine", "Røes", "joro0003@stud.kea.dk", "1234abcd");
+//        profile = new Profile(1, "Test FirstName", "Test LastName", "test@mail.dk", "testpassword");
 //    }
-
-
-}
+//
+//    @Test
+//    public void getProfileByIdTest() throws Exception { //skal vi have den her, vi har jo ikke rigtig getProfileById i controller?
+//        when(profileService.getProfileById(1)).thenReturn(profile);
+//
+//        mockMvc.perform(get("/")) //sætte endpoint ind
+//                .andExpect(status().isOk())
+//                .andExpect(view().name("")) //sætte HTML side ind
+//                .andExpect(model().attributeExists("profile"))
+//                .andExpect(model().attribute("profile", profile));
+//
+//    }
+//
+//}
--- src/test/java/org/example/proflow/serviceTest/ProfileServiceTest.java
+++ src/test/java/org/example/proflow/serviceTest/ProfileServiceTest.java
@@ -1,4 +1,64 @@
 package org.example.proflow.serviceTest;
 
+import org.example.proflow.exception.ProfileException;
+import org.example.proflow.model.Profile;
+import org.example.proflow.repository.ProfileRepository;
+import org.example.proflow.service.ProfileService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class ProfileServiceTest {
+
+    @Mock
+    private ProfileRepository profileRepository;
+
+    @InjectMocks
+    private ProfileService profileService;
+
+    private Profile profile;
+
+    @Before //arrange
+    public void setUp() {
+        profile = new Profile("FirstNameTest", "LastNameTest", "test@example.com", "testpassword");
+    }
+
+    @Test
+    public void getProfileByIdTest() throws SQLException, ProfileException {
+        when(profileRepository.getProfileById(1)).thenReturn(profile);
+
+        //act
+        Profile result = profileService.getProfileById(1);
+
+        //assert
+        assertNotNull(result);
+        assertEquals("FirstNameTest", result.getFirstName());
+        assertEquals("LastNameTest", result.getLastName());
+        assertEquals("test@example.com", result.getEmail());
+        assertEquals("testpassword", result.getPassword());
+    }
+
+
+//    @Test //TODO fra pdf, men det giver ikke mening fordi vi ikke har saveProfile i rep?
+//    public void testAddProfileCallsSaveProfile(){
+//        //Act
+//        profileService.addProfile(profile);
+//
+//        //Assert
+//        verify(profileRepository).saveProfile(profile);
+//
+//    }
+
+
 }
