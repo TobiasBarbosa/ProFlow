f0f40f3c25ce8c274f589dcf94b4bdd1c6f453be
Vahablotfi <vahablotfi1@gmail.com>
2024-12-11 23:54:07 +0100

    interfaces added to util.

---
+++ src/main/java/org/example/proflow/util/interfaces/ProfileRepositoryInterface.java
@@ -0,0 +1,34 @@
+package org.example.proflow.util.interfaces;
+
+import org.example.proflow.exception.ProfileException;
+import org.example.proflow.model.Profile;
+import org.example.proflow.model.Project;
+
+import java.util.List;
+
+public interface ProfileRepositoryInterface {
+
+    // Create a new profile
+    void addProfile(Profile profile);
+
+    // Read a profile by its ID
+    Profile getProfileById(int id) throws ProfileException;
+
+    // Read a profile by its email and password
+    Profile getProfileByEmailAndPassword(String profileEmail, String profilePassword) throws ProfileException;
+
+    // Read all profiles
+    List<Profile> getAllProfiles() throws ProfileException;
+
+    // Get all projects associated with a profile by profile ID
+    List<Project> getProjectsFromProfile(int profileId) throws ProfileException;
+
+    // Update an existing profile
+    void updateProfile(Profile profile);
+
+    // Delete a profile by its ID
+    void deleteProfile(int id);
+
+    // Delete all profiles (for testing purposes only)
+    void deleteAllProfiles();
+}
---
+++ src/main/java/org/example/proflow/util/interfaces/TaskRepositoryInterface.java
@@ -0,0 +1,26 @@
+package org.example.proflow.util.interfaces;
+
+import org.example.proflow.model.Task;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface TaskRepositoryInterface {
+    // Create a new task
+    void addTask(Task task) throws SQLException;
+
+    // Get all tasks
+    List<Task> getAllTasks();
+
+    // Get a task by its ID
+    Task getTaskById(int id) throws SQLException;
+
+    // Update an existing task
+    void updateTask(Task task) throws SQLException;
+
+    // Delete a task by its ID
+    void deleteTask(int taskId) throws SQLException;
+
+    // Delete all tasks (for testing purposes)
+    void deleteAllTasks();
+}
