5a75afe3d1b4cce59dc2b6703a517f518a711f83
MonirMooghen <momo0003@stud.kea.dk>
2024-12-16 15:57:16 +0100

    Add_project og dashboard opdateret.

--- src/main/java/org/example/proflow/controller/ProjectController.java
+++ src/main/java/org/example/proflow/controller/ProjectController.java
@@ -32,7 +32,7 @@ public class ProjectController {
     }
 
     //***CREATE PROJECT METHODS***-----------------------------------------------------------------------------------
-    @GetMapping("/add-project/{profileId}")
+    @GetMapping("/add-project/")
     public String addProject(@PathVariable("profileId") int profileId, Model model, HttpSession session) {
         if (!Validator.isValid(session, profileId)) {
             return "redirect:/";
